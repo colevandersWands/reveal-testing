3752973613660588993089b2f97f8f2409c07f69
colevandersWands <evan.t.cole@gmail.com>
2017-12-20 21:11:16 +0100

    publishing

---
+++ publish.sh
@@ -0,0 +1,21 @@
+# save your work just in case
+git add .
+git commit -m 'publishing'
+
+# pull changes from colevanders
+git pull origin GI
+
+# merge changes into gh-pages
+git checkout gh-pages
+git merge GI
+
+# push changes live
+git add . 
+git commit -m 'publishing'
+git push elium gh-pages
+
+# return to keep working
+git checkout GI
+
+
+# chmod a+x filename
